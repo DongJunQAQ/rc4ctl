c5b94c6b6876a87a928062a12a10cc22fab3937a
DongJun <you@example.com>
2025-11-08 11:39:25 +0800

    fix: 修复了子命令必选flag没起作用的问题

--- cmd/decrypt.go
+++ cmd/decrypt.go
@@ -2,14 +2,12 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
+	"os"
 	"rc4ctl/internal"
 )
 
 func init() {
 	rootCmd.AddCommand(decryptCmd)
-	// 标记此子命令的flag参数为必填，未传则报错，根命令不受影响
-	_ = rootCmd.MarkPersistentFlagRequired("key")
-	_ = rootCmd.MarkPersistentFlagRequired("output")
 }
 
 var decryptCmd = &cobra.Command{ //定义解密子命令
@@ -17,6 +15,16 @@ var decryptCmd = &cobra.Command{ //定义解密子命令
 	Short: "使用RC4算法解密文件",
 	Long:  `将通过encrypt子命令加密的文件解密，恢复为原始可查看的图片文件（需使用加密时的相同密钥）`,
 	Args:  cobra.ExactArgs(1), // 此子命令后面必须传入1个位置参数：输入要解密的文件路径
+	PreRun: func(cmd *cobra.Command, args []string) { // 在执行前检查必需参数
+		if keyFlag == "" || outputFlag == "" {
+			cmd.Printf("错误：必须提供密钥和输出文件路径\n")
+			err := cmd.Usage()
+			if err != nil {
+				return
+			}
+			os.Exit(1)
+		}
+	},
 	Run: func(_ *cobra.Command, args []string) { // 解密逻辑执行函数，args的值为位置参数即需要解密的文件路径
 		internal.ProcessCrypt(args, "解密", outputFlag, keyFlag)
 	},
--- cmd/encrypt.go
+++ cmd/encrypt.go
@@ -2,14 +2,12 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
+	"os"
 	"rc4ctl/internal"
 )
 
 func init() { // 将加密子命令添加到根命令
 	rootCmd.AddCommand(encryptCmd)
-	// 标记此子命令的flag参数为必填，未传则报错，根命令不受影响
-	_ = rootCmd.MarkPersistentFlagRequired("key")
-	_ = rootCmd.MarkPersistentFlagRequired("output")
 }
 
 var encryptCmd = &cobra.Command{ //定义加密子命令
@@ -17,6 +15,18 @@ var encryptCmd = &cobra.Command{ //定义加密子命令
 	Short: "使用RC4算法加密文件",
 	Long:  `将指定图片文件通过RC4算法加密，输出为不可直接查看的文件（需用decrypt子命令解密）`,
 	Args:  cobra.ExactArgs(1), // 此子命令后面必须传入1个位置参数：输入要加密的文件路径
+	PreRun: func(cmd *cobra.Command, args []string) { // Cobra提供的钩子函数，会在命令的Run函数执行前自动调用，即在执行前检查必需参数
+		//cmd: 当前正在执行的命令对象（如encrypt或decrypt子命令）
+		//args: 命令接收的位置参数（如输入文件路径）
+		if keyFlag == "" || outputFlag == "" {
+			cmd.Printf("错误：必须提供密钥和输出文件路径\n") //若某一参数为空，则打印错误提示
+			err := cmd.Usage()               //打印当前命令的使用帮助
+			if err != nil {
+				return
+			}
+			os.Exit(1) //若校验参数失败则以状态码1退出程序
+		}
+	},
 	Run: func(_ *cobra.Command, args []string) { // 加密逻辑执行函数，args的值为位置参数即需要加密的文件路径
 		internal.ProcessCrypt(args, "加密", outputFlag, keyFlag)
 	},
